c5108e49896b3d6eaff02d72c6f7c83c31e117f6
Shivam Yadav <shivam20yadav@gmail.com>
2020-08-17 13:04:21 +0530

    commit

--- page/insert.php
+++ page/insert.php
@@ -7,20 +7,22 @@ if(!isset($_SESSION["sess_name"])){
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
-	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    	<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Kairav</title>
-		<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:700, 600,500,400,300' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-		
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Insert</title>
+	<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:700, 600,500,400,300' rel='stylesheet'
+		type='text/css'>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
-		<script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
-		<script src="https://code.highcharts.com/highcharts.js"></script>
-		<script src="https://code.highcharts.com/modules/data.js"></script>
+	<script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+	<script src="https://code.highcharts.com/highcharts.js"></script>
+	<script src="https://code.highcharts.com/modules/data.js"></script>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 		<style type="text/css">
-			* {
+		* {
 			box-sizing: border-box;
 		}
 		body {
@@ -238,11 +240,14 @@ if(!isset($_SESSION["sess_name"])){
 		tr:nth-child(even) {
 		background-color: #dddddd;
 		}
-
-
-
-		</style>
-
+	</style>
+	<script>
+		$(document).ready(function() {
+		$('.nav-trigger').click(function() {
+		$('.side-nav').toggleClass('visible');
+	});
+});
+	</script>
 	</head>
 	<body>
 		<div class="header">
@@ -272,6 +277,11 @@ if(!isset($_SESSION["sess_name"])){
                     <li>
 						<a href="dispatch.php">
 							<span>Dispatch</span>
+						</a>
+					</li>
+					<li>
+						<a href="logout.php">
+							<span>Logout</span>
 						</a>
 					</li>
 				</ul>
