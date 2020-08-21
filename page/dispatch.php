0c02113afe82364dde05991e38e79fecc8d6a038
Shivam Yadav <shivam20yadav@gmail.com>
2020-08-21 15:42:03 +0530

    commit

--- page/dispatch.php
+++ page/dispatch.php
@@ -20,7 +20,7 @@ if(!isset($_SESSION["sess_name"])){
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-		<style type="text/css">
+	<style type="text/css">
 		* {
 			box-sizing: border-box;
 		}
